2b45ab3637b97f6d3a7b2a65d3f41fee951f9f59
anthony cantor <ar.cantor@gmail.com>
2013-10-26 18:58:12 -0600

    updated tests to match code changes

--- test/Data/CycleRoll/Test.hs
+++ test/Data/CycleRoll/Test.hs
@@ -13,33 +13,15 @@ import Test.HUnit
 
 group = 
   testGroup "CycleRoll" [
-    example_group
+    basic_group
     ]
 
-example_group = 
+basic_group = 
   testGroup "example" [
-    testCase        "suffix array"      suffix_array,
-    testCase        "lcp array"         lcp_array,
-    testCase        "sorted lcp"        sorted_lcp,
-    testCase        "pos diffs"         pos_diffs
+    testCase        "suffix array"      suffix_array
     ]
   where
     input   = fromList "ababcabab\0"
     sa      = SA.make input
-    lcp_arr = LCP.array input sa
-    sorted  = LCP.sorted lcp_arr
-    pdiffs  = CR.posDiffs sa sorted
 
     suffix_array = (fromList [9,7,5,0,2,8,6,1,3,4]) @=? sa
-
-    lcp_array = 
-      expected @=? lcp_arr
-      where
-        expected = fromList [(0,0),(2,1),(4,2),(2,3),(0,4),(1,5),(3,6),(1,7),(0,8)]
-
-    sorted_lcp = 
-      expected @=? sorted
-      where
-        expected = fromList [(4,2),(3,6),(2,1),(2,3),(1,5),(1,7),(0,0),(0,4),(0,8)]
-
-    pos_diffs = fromList [5,5,2,2,2,2,2,6,1] @=? pdiffs
